2c22c832418e468349c664e801bb4bb37f2e6dd6
karthikeyan kumbam <karthikeyankumbam123@gmail.com>
2023-07-13 21:01:33 +0530

    added db script and exception in customer service

--- Backend/customer-ms/src/main/java/com/personal/customerms/dto/CustomerDTO.java
+++ Backend/customer-ms/src/main/java/com/personal/customerms/dto/CustomerDTO.java
@@ -1,4 +1,68 @@
 package com.personal.customerms.dto;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
+
 public class CustomerDTO {
+    private Integer id;
+    @NotNull
+    private String fullName;
+    @NotNull
+    private String email;
+    @NotNull
+    private  String username;
+    @NotNull
+    private String password;
+
+    private Boolean status;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Boolean getStatus() {
+        return status;
+    }
+
+    public void setStatus(Boolean status) {
+        this.status = status;
+    }
 }
